8965d570d4ee22e78d2ad499ff2055a4fd666dd4
Kita <ankitashah212@gmail.com>
2017-11-04 12:12:11 -0400

    migrated categories and posts models

--- lemongrassSharing/app/Post.php
+++ lemongrassSharing/app/Post.php
@@ -4,12 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Post extends Model
 {
-    public function posts() {
-        return $this->hasMany('App\Post');
+    public function category() {
+        return $this->belongsTo('App\Category');
     }
     public function user() {
         return $this->belongsTo('App\User');
     }
+  
 }
