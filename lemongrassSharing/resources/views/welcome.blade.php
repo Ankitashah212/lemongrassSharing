73def7c2ef9e508d6ba62fc405769e9310231080
Kita <ankitashah212@gmail.com>
2017-11-06 14:06:32 -0500

    changed index page

--- lemongrassSharing/resources/views/welcome.blade.php
+++ lemongrassSharing/resources/views/welcome.blade.php
@@ -5,16 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Lemongrass</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
+        <link href="https://fonts.googleapis.com/css?family=Marck+Script" rel="stylesheet">       
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background: url("lemongrass.jpg") no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+                background-color: #C6E391;
+                color: #212001;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
                 height: 100vh;
@@ -50,24 +54,33 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #212001;
                 padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
+                font-size: 16px;
+                font-weight: 900;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
 
             .m-b-md {
-                margin-bottom: 30px;
+                font-family: 'Marck Script', cursive;            
             }
+
+            .punchline {
+                color: #212001;
+                font-weight: bold;
+                font-size: 20px;
+                font-family: 'Marck Script', cursive;            
+
+            }
+
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+                <div class="top-right links ">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -79,15 +92,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lemongrass
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="punchline">
+                 Live, Love, Share....
                 </div>
             </div>
         </div>
